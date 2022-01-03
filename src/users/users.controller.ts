29acd0e92fc8ece456b74b2fd8ea653ad93f1a2b
LiliyaGavrishchishena <liliya.gavrishchishena@gmail.com>
2022-01-04 00:27:45 +0200

    work with session

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,20 +8,41 @@ import {
   Param,
   Query,
   NotFoundException,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersServise: UsersService) {}
+  constructor(
+    private usersServise: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('/colors/:color')
+  setColor(@Param('color') color: string, @Session() session: any) {
+    session.color = color;
+  }
+
+  @Get('/colors')
+  getColor(@Session() session: any) {
+    return session.color;
+  }
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersServise.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
 
   @Get('/:id')
